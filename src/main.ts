b66ea5ea584d8fbbcdfcb8cd750a8fb950d0316f
hoangminh122 <hoangminh12297@gmail.com>
2021-05-24 02:44:01 +0700

    custom filter exception

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { DispatchError } from './shared/filters/dispatch-error';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //filter exception
+  app.useGlobalFilters(new DispatchError())
+
+  //swagger config
   const config = new DocumentBuilder()
   .setTitle("trello")
   .setDescription("The trello API description")
@@ -18,6 +23,7 @@ async function bootstrap() {
   SwaggerModule.setup('api',app,document);
   app.enableCors(); //protection
 
+  //validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -28,6 +34,7 @@ async function bootstrap() {
     }),
   );
   
+  //server
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
